b1db163d221b2e4bc3d5ac7df8c61a8ea37e943c
زيد <zayed419855@fci.bu.edu.eg>
2024-09-30 22:37:30 +0300

    git commit -m "AddProductAndOrderTables"

---
+++ ECommerce_Management_MVC/Models/OrderDetail.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce_Management_MVC.Models
+{
+    public class OrderDetail
+    {
+        [Key]
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        [ForeignKey("pr")]
+        public int ProductId { get; set; }
+        public decimal Price { get; set; }
+        public string Sku { get; set; }
+        public int Quantity { get; set; }
+        public Product pr { get; set; }
+    }
+}
---
+++ ECommerce_Management_MVC/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce_Management_MVC.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Sku { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public float Weight { get; set; }
+        public string Descriptions { get; set; }
+        public string Thumbnail { get; set; }
+        public string Image { get; set; }
+        public string Category { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int Stock { get; set; }
+        public virtual List<OrderDetail> orderDetails { get; set; } = new List<OrderDetail>();
+    }
+}
