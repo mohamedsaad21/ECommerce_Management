fdf5f530b4a6afbac6ff0313ebdbb2db36c38d90
محمد <mohammed419965@fci.bu.edu.eg>
2024-09-30 21:06:27 +0300

    git commit -m "addCustomersandOrders"

---
+++ ECommerce_Management_MVC/Models/Orders.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECommerce_Management_MVC.Models
+{
+    public class Orders
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("customer")]
+        public int Customer_Id { get; set; }
+        public int amount { get; set; }
+        public string Shipping_Address  { get; set; }
+        public string Order_Address { get; set; }
+        public string Order_Email { get; set; }
+        public string Order_Date { get; set; }
+        public string Order_Status { get; set; }
+
+        public virtual Customers customer { set; get; }
+    }
+}
