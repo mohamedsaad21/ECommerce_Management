79fa3131c5ee67066bb7ad2bba5bcfad8fbb9754
mohamedsaad21 <mohamedsaad2756@gmail.com>
2024-10-05 16:55:30 +0300

    git commit -m "FinishTables"

--- ECommerce_Management_MVC/Program.cs
+++ ECommerce_Management_MVC/Program.cs
@@ -21,8 +21,8 @@ namespace ECommerce_Management_MVC
             builder.Services.AddControllersWithViews();
 
             builder.Services.AddScoped<IEntityRepository<Product>, ProductRepository>();
-            builder.Services.AddScoped<IEntityRepository<Category>,  CategoryRepository>();
-            builder.Services.AddScoped<IEntityRepository<Order>,  OrderRepository>();
+            builder.Services.AddScoped<IEntityRepository<Category>, CategoryRepository>();
+            builder.Services.AddScoped<IEntityRepository<Order>, OrderRepository>();
             builder.Services.AddScoped<IEntityRepository<product_category>, Product_CategoriesRepository>();
             builder.Services.AddScoped<IEntityRepository<OrderDetail>, Order_DetailsRepository>();
 
@@ -42,51 +42,8 @@ namespace ECommerce_Management_MVC
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
-
-
+            
             app.Run();
-        }
-    }
-    public static class RoleHelper
-    {
-        public static void AddRole(this IServiceProvider services, string v)
-        {
-            // ijnect explicitly RoleManager
-            var RoleManger = services.CreateScope().ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-            // create variable to take result of operation
-            IdentityResult result;
-            // check if role not exist
-            if (!RoleManger.RoleExistsAsync(v).Result)
-            {
-                // create role using new IdentityRole
-                result = RoleManger.CreateAsync(new IdentityRole(v)).Result;
-            }
-        }
-        public static void AddAdminRoles(this IServiceProvider serviceProvider)
-        {
-            // inject explicitly UserManager and RoleManager
-            var userManger = serviceProvider.CreateScope().ServiceProvider.GetRequiredService<UserManager<Customer>>();
-            var roleManger = serviceProvider.CreateScope().ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-            // inital result to recive result of operation
-            IdentityResult result;
-            // ensure from admin befor add role to him
-            var user = userManger.FindByEmailAsync("Admin@gmail.com")?.Result;
-            if (user == null)
-            {
-                user = new Customer() { FullName = "Admin", UserName = "Admin", Email = "Admin@gmail.com" };
-                var resul = userManger.CreateAsync(user, "Ad@123").Result;
-                if (!resul.Succeeded)
-                {
-                    throw new ArgumentException("admin not create d");
-                }
-            }
-
-            // add every role to admin
-            roleManger.Roles.ToList().ForEach(r =>
-            {
-
-                result = userManger.AddToRoleAsync(user, r.Name).Result;
-            });
         }
     }
 }
